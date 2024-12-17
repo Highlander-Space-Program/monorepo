ef2cd5784c9bea92e873a0397b1771d2d92c1e18
neeleshroy06 <neelesh.roy06@gmail.com>
2024-12-17 15:23:52 -0500

    Update canalyzer_ui.py

--- canalyzer_ui/canalyzer_ui.py
+++ canalyzer_ui/canalyzer_ui.py
@@ -1,13 +1,19 @@
 import dearpygui.dearpygui as dpg
-dpg.create_context()
+from screeninfo import get_monitors
 
-USB_list=[1,2,3,4,5,6,7,9]
+dpg.create_context()
 
 def printValue(Sender):
     print(dpg.get_value(Sender))
 
+def get_screen_resolution():
+    monitor = get_monitors()[0]
+    return monitor.width, monitor.height
+
 def main():
-    with dpg.window(label="Send Message to USB Port",width=600,height=400):
+    s_width, s_height = get_screen_resolution()
+    
+    with dpg.window(label="Send Message to USB Port",width=s_width, height=s_height):
         dpg.add_text("Select USB Port")
         dpg.add_combo((1,2,3,4,5,6,7,8),callback=printValue)
         dpg.add_text("Enter message to send:")
@@ -15,7 +21,7 @@ def main():
         dpg.add_button(label="Send Message")
     
 
-    dpg.create_viewport(title="Send Message", width=1000,height=600)
+    dpg.create_viewport(title="Send Message", width=s_width, height=s_height)
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
